162c868207ea1789ab2b2c4da17c3f86b41fecb0
chenxiahua123 <282559082@qq.com>
2019-03-14 19:00:12 +0800

    首页美观 完成

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Wheel(models.Model):
+    name=models.CharField(max_length=100)
+    img=models.CharField(max_length=100)
+
+    class Meta:
+        db_table='网易考拉——轮播图'
+
+class Tuijian(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = '网易考拉——推荐'
--- app/views.py
+++ app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import Wheel, Tuijian
+
+
 def index(request):
-    return render(request,'index.html')
+
+
+    wheels=Wheel.objects.all()
+
+
+    tuijians=Tuijian.objects.all()
+
+    print(tuijians)
+
+
+
+
+    data={
+        'wheels':wheels,
+        'tuijians':tuijians,
+    }
+
+    return render(request,'index.html',context=data)
